214d40752b96dbfb80ef2452e132ce49bff2b43d
Shaoxiaoze <shaoxiaoze@hotmail.com>
2018-06-25 10:24:06 +0800

    fx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div sytle={{width: '10px'}}>
+                    <img src={require('./logo.svg')}/>
+                </div>
+                <Tabs>
+                    <TabList style={{
+                        display: 'flex', flexDirection: 'row',
+                        justifyContent: 'space-around'
+                    }}>
+                        <Tab>题目 1</Tab>
+                        <Tab>题目 2</Tab>
+                        <Tab>题目 3</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <h2>详情 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>详情 2</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>详情 3</h2>
+                    </TabPanel>
+                </Tabs>
+            </div>
+        );
+    }
 }
 
 export default App;
